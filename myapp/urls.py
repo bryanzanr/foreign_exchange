30f81c7614a2d78c31e1742bea3e70dcf4a63bbc
Adityawarman Fanaro <reznov53@live.com>
2018-03-28 14:19:04 +0700

    [GREEN] Attempt to fix application error on heroku

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,21 +1,21 @@
-from django.urls import path
+# from django.urls import path
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello, name='hello'),
-    path('register/', views.register, name='register'),
+    url(r'hello/', views.hello, name='hello'),
+    url(r'register/', views.register, name='register'),
     url(r'register/$', views.register, name='register'),
-    path('header/', views.header, name='header'),
-    path('footer/', views.footer, name='footer'),
+    url(r'header/', views.header, name='header'),
+    url(r'footer/', views.footer, name='footer'),
     url(r'^$', views.login, name='login'),
     url(r'login/', views.login, name='login'),
-    # path('auth/', views.auth_view, name='auth'),
-    path('logout/', views.logout, name='logout'),
-    path('loggedin/', views.loggedin, name='loggedin'),
-    path('registered/', views.registered, name='registered'),
-    path('invalid/', views.invalid_login, name='invalid'),
+    # url(r'auth/', views.auth_view, name='auth'),
+    url(r'logout/', views.logout, name='logout'),
+    url(r'loggedin/', views.loggedin, name='loggedin'),
+    url(r'registered/', views.registered, name='registered'),
+    url(r'invalid/', views.invalid_login, name='invalid'),
 ]
 
 # urlpatterns += patterns(”, (r’^static/(?P.*)$’,
