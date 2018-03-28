61da7e08ce574ba4d5a05e5aa6cb7bd8cff5046b
Bryanza Novirahman <bryanzanr@gmail.com>
2018-03-28 11:09:25 +0700

    [GREEN]Static Configuration

--- flyit/settings.py
+++ flyit/settings.py
@@ -128,8 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'myapp/static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
